36ceebf11464203731053d2ff6f13d923ff7ac89
tsadakane <40597344+tsadakane@users.noreply.github.com>
2024-02-05 11:08:54 +0900

    Cache 使用時の CUDA 環境変数設定

--- helper/SetupCUDASDK.ps1
+++ helper/SetupCUDASDK.ps1
@@ -3,7 +3,9 @@ param (
     [string]$cuda,
 
     [Parameter(Mandatory=$true)]
-    [string]$download
+    [string]$download,
+
+    [string]$skipInstall = "false"
 )
 
 $path_download = $download
@@ -30,81 +32,84 @@ if ($CUDA_KNOWN_URLS.ContainsKey($cuda_ver)){
     $cuda_ver_short = $cuda_ver_major+"."+$cuda_ver_minor
 }
 
-$list_split_url = $url.Split('/')
-$filename = $list_split_url[-1]
-$dir_name_dl = $path_download
-$outputPath = $dir_name_dl + "/" + $filename
-if (!(Test-Path -Path $dir_name_dl)) {
-  New-Item -ItemType directory -Path $dir_name_dl
-}
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-if (!(Test-Path -Path $outputPath -PathType leaf)) {
-    Write-Output "File not found: "
-    Write-Output $outputPath
+if (-not ($skipInstall == "true")) {
+    $list_split_url = $url.Split('/')
+    $filename = $list_split_url[-1]
+    $dir_name_dl = $path_download
+    $outputPath = $dir_name_dl + "/" + $filename
+    if (!(Test-Path -Path $dir_name_dl)) {
+    New-Item -ItemType directory -Path $dir_name_dl
+    }
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+    if (!(Test-Path -Path $outputPath -PathType leaf)) {
+        Write-Output "File not found: "
+        Write-Output $outputPath
+    }
+
+    $list_install_option=@(
+        "cuda_profiler_api_"+$cuda_ver_short,
+        "cudart_"+$cuda_ver_short,
+        "cuobjdump_"+$cuda_ver_short,
+        "cupti_"+$cuda_ver_short,
+        "cuxxfilt_"+$cuda_ver_short,
+        # "demo_suite_"+$cuda_ver_short,
+        # "documentation_"+$cuda_ver_short,
+        "memcheck_"+$cuda_ver_short,
+        "nvcc_"+$cuda_ver_short,
+        "nvdisasm_"+$cuda_ver_short,
+        "nvml_dev_"+$cuda_ver_short,
+        "nvprof_"+$cuda_ver_short,
+        "nvprune_"+$cuda_ver_short,
+        "nvrtc_"+$cuda_ver_short,
+        "nvrtc_dev_"+$cuda_ver_short,
+        "nvtx_"+$cuda_ver_short,
+        # "visual_profiler_"+$cuda_ver_short,
+        "sanitizer_"+$cuda_ver_short,
+        "thrust_"+$cuda_ver_short,
+        "cublas_"+$cuda_ver_short,
+        "cublas_dev_"+$cuda_ver_short,
+        "cufft_"+$cuda_ver_short,
+        "cufft_dev_"+$cuda_ver_short,
+        "curand_"+$cuda_ver_short,
+        "curand_dev_"+$cuda_ver_short,
+        "cusolver_"+$cuda_ver_short,
+        "cusolver_dev_"+$cuda_ver_short,
+        "cusparse_"+$cuda_ver_short,
+        "cusparse_dev_"+$cuda_ver_short,
+        "npp_"+$cuda_ver_short,
+        "npp_dev_"+$cuda_ver_short,
+        "nvjpeg_"+$cuda_ver_short,
+        "nvjpeg_dev_"+$cuda_ver_short,
+        # "nsight_compute_"+$cuda_ver_short,
+        # "nsight_nvtx_"+$cuda_ver_short,
+        # "nsight_systems_"+$cuda_ver_short,
+        # "nsight_vse_"+$cuda_ver_short,
+        "visual_studio_integration_"+$cuda_ver_short
+        # "occupancy_calculator_"+$cuda_ver_short,
+    )
+
+    $option_string = [string]::Join(" ", $list_install_option)
+    $option_string = "-s "+ $option_string
+    Write-Host $list_install_option
+
+    $process = Start-Process -FilePath $outputPath -ArgumentList $option_string -Wait -PassThru
+    if ($process.ExitCode -ne 0) {
+        Write-Output "app.exe exited with code $($process.ExitCode)"
+    }
+    if (!$?) {
+        Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
+        exit 1 
+    }
+    Write-Host "Phase 1: CUDA_PATH = $($CUDA_PATH)"
 }
 
-$list_install_option=@(
-    "cuda_profiler_api_"+$cuda_ver_short,
-    "cudart_"+$cuda_ver_short,
-    "cuobjdump_"+$cuda_ver_short,
-    "cupti_"+$cuda_ver_short,
-    "cuxxfilt_"+$cuda_ver_short,
-    # "demo_suite_"+$cuda_ver_short,
-    # "documentation_"+$cuda_ver_short,
-    "memcheck_"+$cuda_ver_short,
-    "nvcc_"+$cuda_ver_short,
-    "nvdisasm_"+$cuda_ver_short,
-    "nvml_dev_"+$cuda_ver_short,
-    "nvprof_"+$cuda_ver_short,
-    "nvprune_"+$cuda_ver_short,
-    "nvrtc_"+$cuda_ver_short,
-    "nvrtc_dev_"+$cuda_ver_short,
-    "nvtx_"+$cuda_ver_short,
-    # "visual_profiler_"+$cuda_ver_short,
-    "sanitizer_"+$cuda_ver_short,
-    "thrust_"+$cuda_ver_short,
-    "cublas_"+$cuda_ver_short,
-    "cublas_dev_"+$cuda_ver_short,
-    "cufft_"+$cuda_ver_short,
-    "cufft_dev_"+$cuda_ver_short,
-    "curand_"+$cuda_ver_short,
-    "curand_dev_"+$cuda_ver_short,
-    "cusolver_"+$cuda_ver_short,
-    "cusolver_dev_"+$cuda_ver_short,
-    "cusparse_"+$cuda_ver_short,
-    "cusparse_dev_"+$cuda_ver_short,
-    "npp_"+$cuda_ver_short,
-    "npp_dev_"+$cuda_ver_short,
-    "nvjpeg_"+$cuda_ver_short,
-    "nvjpeg_dev_"+$cuda_ver_short,
-    # "nsight_compute_"+$cuda_ver_short,
-    # "nsight_nvtx_"+$cuda_ver_short,
-    # "nsight_systems_"+$cuda_ver_short,
-    # "nsight_vse_"+$cuda_ver_short,
-    "visual_studio_integration_"+$cuda_ver_short
-    # "occupancy_calculator_"+$cuda_ver_short,
-)
-
-$option_string = [string]::Join(" ", $list_install_option)
-$option_string = "-s "+ $option_string
-Write-Host $list_install_option
-
-$process = Start-Process -FilePath $outputPath -ArgumentList $option_string -Wait -PassThru
-if ($process.ExitCode -ne 0) {
-    Write-Output "app.exe exited with code $($process.ExitCode)"
-}
-if (!$?) {
-    Write-Output "Error: CUDA installer reported error. $($LASTEXITCODE)"
-    exit 1 
-}
-Write-Host "Phase 1: CUDA_PATH = $($CUDA_PATH)"
 # CUDA のインストールパス
 $CUDA_PATH = "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$($cuda_ver_major).$($cuda_ver_minor)"
 Write-Host "Phase 2: CUDA_PATH = $($CUDA_PATH)"
 $env:CUDA_PATH="$($CUDA_PATH)"
 
-$path_nvcc = "$($CUDA_PATH)\bin\nvcc.exe"
-Write-Output "path_nvcc: " $path_nvcc
+# $path_nvcc = "$($CUDA_PATH)\bin\nvcc.exe"
+# Write-Output "path_nvcc: " $path_nvcc
 # Start-Process -FilePath $path_nvcc -ArgumentList "-V" -NoNewWindow -Wait
 
 $env:envname_CUDA_PATH_VX_Y = "CUDA_PATH_V$($cuda_ver_major)_$($cuda_ver_minor)" 
